311cf8e767abbae078193dc75f39cc0926acb276
Sylkdev <sylkdev@gmail.com>
2025-04-24 14:54:19 +0200

    finalisation route

--- public/index.php
+++ public/index.php
@@ -2,7 +2,12 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,10 +11,12 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserController extends AbstractController
 {
+
     #[Route('/api/me', name: 'api_me', methods: ['GET'])]
     #[IsGranted('ROLE_USER')]
     public function me(SerializerInterface $serializer, UserInterface $user): JsonResponse
     {
+
         $data = $serializer->serialize($user, 'json', ['groups' => ['user:read']]);
         return new JsonResponse($data, 200, [], true);
     }
