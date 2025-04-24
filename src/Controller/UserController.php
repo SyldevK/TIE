356258a685b48322dee5257b3bcd56104fc744ae
Sylkdev <sylkdev@gmail.com>
2025-04-24 11:03:37 +0200

    ajout route me

---
+++ src/Controller/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserController extends AbstractController
+{
+    #[Route('/api/me', name: 'api_me', methods: ['GET'])]
+    #[IsGranted('ROLE_USER')]
+    public function me(SerializerInterface $serializer, UserInterface $user): JsonResponse
+    {
+        $data = $serializer->serialize($user, 'json', ['groups' => ['user:read']]);
+
+        return new JsonResponse($data, 200, [], true);
+    }
+}
